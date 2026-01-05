60ae4371b04eae230fa58a7316fa285814790897
mEthan-e <eathomas1012@gmail.com>
2026-01-05 20:22:13 +0000

    Running

--- BrainlessJam/Assets/Scripts/PlayerMovement.cs
+++ BrainlessJam/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Values")]
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeedBase = 5f;
     [SerializeField] float jumpHeight = 5f;
     [SerializeField] float rotateSpeed = 120f;
+    [SerializeField] float runBoost = 5f;
 
     Animator animator;
 
@@ -22,15 +25,24 @@ public class PlayerMovement : MonoBehaviour
     void ProcessMovement()
     {
         float horizontal = Input.GetAxis("Horizontal"); 
-        float vertical = Input.GetAxis("Vertical");     
+        float vertical = Input.GetAxis("Vertical");
+        float moveSpeed = moveSpeedBase; 
 
         
         if (animator != null)
         {
-            animator.SetBool("IsWalking", vertical > 0.3 || vertical < -0.3 || 
-            horizontal > 0.3 || horizontal < -0.3);
+            animator.SetBool("IsWalking", (vertical > 0.3 || vertical < -0.3 || 
+            horizontal > 0.3 || horizontal < -0.3) && !Keyboard.current.shiftKey.isPressed);
         }
-
+        if (Keyboard.current.shiftKey.isPressed)
+        {
+            moveSpeed += runBoost;
+        }
+        else
+        {
+            moveSpeed = moveSpeedBase;
+        }
+        animator.SetBool("IsRunning", Keyboard.current.shiftKey.isPressed);
         
         transform.Translate(0f, 0f, vertical * moveSpeed * Time.deltaTime);
 
