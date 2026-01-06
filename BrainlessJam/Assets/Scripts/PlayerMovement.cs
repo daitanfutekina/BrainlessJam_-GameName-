4b1fee0ffe5bd9f39cd49f5c051109838ced6bb4
mEthan-e <eathomas1012@gmail.com>
2026-01-06 15:24:12 +0000

    Map Preperations

--- BrainlessJam/Assets/Scripts/PlayerMovement.cs
+++ BrainlessJam/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,11 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Values")]
-    [SerializeField] float moveSpeedBase = 5f;
+    [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpHeight = 5f;
     [SerializeField] float rotateSpeed = 120f;
-    [SerializeField] float runBoost = 5f;
-
-    [Header("Particles")]
-    [SerializeField] ParticleSystem walkParticles;
 
     Animator animator;
 
@@ -28,37 +22,17 @@ public class PlayerMovement : MonoBehaviour
     void ProcessMovement()
     {
         float horizontal = Input.GetAxis("Horizontal"); 
-        float vertical = Input.GetAxis("Vertical");
-        float moveSpeed = moveSpeedBase; 
+        float vertical = Input.GetAxis("Vertical");     
 
         
         if (animator != null)
         {
-            animator.SetBool("IsWalking", (vertical > 0.3 || vertical < -0.3 || 
-            horizontal > 0.3 || horizontal < -0.3) && !Keyboard.current.shiftKey.isPressed);
+            animator.SetBool("IsWalking", vertical > 0.3 || vertical < -0.3 || 
+            horizontal > 0.3 || horizontal < -0.3);
         }
-        if (Keyboard.current.shiftKey.isPressed)
-        {
-            moveSpeed += runBoost;
-        }
-        else
-        {
-            moveSpeed = moveSpeedBase;
-        }
-        animator.SetBool("IsRunning", Keyboard.current.shiftKey.isPressed);
+
         
         transform.Translate(0f, 0f, vertical * moveSpeed * Time.deltaTime);
-        if (vertical > 0.3 || vertical < -0.3 || horizontal > 0.3 || horizontal < -0.3)
-        {
-            if (!walkParticles.isPlaying)
-            {
-                walkParticles.Play();
-            }
-        }
-        else
-        {
-            walkParticles.Stop();
-        }
 
         
         transform.Rotate(0f, horizontal * rotateSpeed * Time.deltaTime, 0f);
