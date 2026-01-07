79453904e6a66e4d5bab7ff238f1faab765b53c6
mEthan-e <eathomas1012@gmail.com>
2026-01-07 20:43:37 +0000

    Spear Hunting Updates
    Final Commit of the day.

---
+++ BrainlessJam/Assets/Scripts/SpearHunting/TargetManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public SpearHuntingManager minigameManager;
+    public GameObject[] targets;
+    public int hitCount = 0;
+}
---
+++ BrainlessJam/Assets/Scripts/SpearHunting/Targets.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Targets : MonoBehaviour
+{
+    [Header("Configuration")]
+    public TargetManager targetManager;
+    public SpearHuntingManager minigameManager;
+
+    [Header("Values")]
+    public float minSpeed;
+    public float maxSpeed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MinigameSpear")
+        {
+            targetManager.hitCount++;
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
